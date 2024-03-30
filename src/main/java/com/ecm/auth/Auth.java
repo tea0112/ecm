4af05210106030866b6494735c64bacd8307ccdc
tea0112 <ducthaidev@gmail.com>
2024-03-30 16:09:11 +0700

    remove unused

--- src/main/java/com/ecm/auth/Auth.java
+++
@@ -1,9 +0,0 @@
-package com.ecm.auth;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/auth")
-public class Auth {
-}
